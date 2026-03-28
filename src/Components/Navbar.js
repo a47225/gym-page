4f2fb098cf8b70abbe6ce32be5f695cd01608784
Diogo Fonseca <102146144+a47225@users.noreply.github.com>
2026-03-28 11:52:22 +0000

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,6 +3,7 @@ import Logo from '../images/Logo.jpg';
 import '../Components/Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
@@ -11,8 +12,8 @@ const Navbar = () => {
             <nav>
                 <img src={Logo} alt="Logo" className="logo" onClick={handleClick}/>
                 <ul className='middle-ul'>
-                    <li><a className="ref-exercises">Search Exercises</a></li>
-                    <li><a href="/food" className='ref-food'>Search Foods</a></li>
+                    <li><Link to="/exercises" className="ref-exercises">Search Exercises</Link></li>
+                    <li><Link to="/food" className='ref-food'>Search Foods</Link></li>
                 </ul>
                 <ul className='login'>
                     <li><a className='icon'><FontAwesomeIcon icon={faUser} style={{color: "#0c1416",fontSize: "2rem", paddingBottom:"10px"}} />Login</a></li>
